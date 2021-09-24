64cfbc170b4eb60c3cb31981049431e070990a9d
Pranay <3pranay@gmail.com>
2021-09-24 19:24:59 +0530

    First Commit

---
+++ src/Prime/primeno.java
@@ -0,0 +1,30 @@
+package Prime;
+
+import java.util.Scanner;
+
+public class primeno {
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		
+		int flag=0;
+		
+		for(int i=2; i<num/2; i++) {
+			
+			if (num % i == 0) {
+				System.out.println("Not Prime Number");
+				flag=1;
+				break;
+			}
+		}
+		
+		if (flag == 0) {
+			System.out.print("Prime Number");
+		}
+	}
+	
+	
+}
+		
